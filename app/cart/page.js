e2c5102ea8b36c8673d5d6621d7f63748b7551a2
Evgeniy Shimkov <yakimoto.evgeniy@gmail.com>
2023-11-09 09:31:36 +0200

    first commit

---
+++ app/cart/page.js
@@ -0,0 +1,15 @@
+import MainLayout from "@/app/layouts/MainLayout";
+import SimilarProducts from "@/app/components/SimilarProducts";
+
+const Cart = () => {
+  return (
+    <div>
+      <MainLayout>
+        <SimilarProducts/>
+      </MainLayout>
+    </div>
+  )
+}
+
+export default Cart;
+
---
+++ app/components/CarouselComp.js
@@ -0,0 +1,10 @@
+const CarouselComp = () => {
+    return (
+    <div>
+    
+    </div>
+    )
+}
+
+export default CarouselComp;
+
